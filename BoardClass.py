d9bc1978e2896824c0be7ea0093eae9db6f22d75
Ethan Bechtol <48114346+EthanBechtol@users.noreply.github.com>
2019-06-28 23:14:23 -0700

    Add set_tile, check_capacity, reset, _generate_blank_rows methods to BoardClass.

--- BoardClass.py
+++ BoardClass.py
@@ -1,11 +1,8 @@
 class Board:
     def __init__(self, size: int = 3):
-        self._tiles = []
+        self._tiles = self._generate_blank_rows(size)
         self._size = size
-
-        blank_row = [None for i in range(size)]
-        for i in range(size):
-            self._tiles.append(blank_row[:])
+        self._moves = 0
 
     def get_board(self):
         return self._tiles
@@ -45,3 +42,45 @@ class Board:
                 if column == (self._size - 1):
                     print("│", end='')
             print()
+
+    def set_tile(self, row: int, column: int, char: str):
+        if self._tiles[row][column] is not None:
+            raise OccupiedSpaceError
+        else:
+            self._tiles[row][column] = char
+            self._moves += 1
+            # TODO check for win here & end game
+
+    def check_capacity(self, verify=False):
+        # Verify parameter inspects each individual space to make sure nothing was changed illegally.
+        if not verify:
+            return (self._size ** 2) - self._moves
+        else:
+            free_spaces = 0
+            for row in self._tiles:
+                for column in row:
+                    if column is None:
+                        free_spaces += 1
+
+            return free_spaces
+
+    def reset(self, size: int or str = 'same'):
+        if size == "same":
+            self._tiles = self._generate_blank_rows(self._size)
+        elif isinstance(size, int):
+            self._tiles = self._generate_blank_rows(size)
+        else:
+            raise ValueError("Invalid argument: {}. Leave argument blank to keep current size or enter a new size int.")
+
+    @staticmethod
+    def _generate_blank_rows(size: int):
+        tiles = []
+        blank_row = [None for i in range(int(size))]
+        for i in range(int(size)):
+            tiles.append(blank_row[:])
+
+        return tiles
+
+
+class OccupiedSpaceError(Exception):
+    pass
