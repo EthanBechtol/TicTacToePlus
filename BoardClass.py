606669783f1c0530eeca1460e97d84a77799e801
Ethan Bechtol <48114346+EthanBechtol@users.noreply.github.com>
2019-06-28 22:30:00 -0700

    Added print_board method with optional coordinate numbers (will be useful for larger boards).

--- BoardClass.py
+++ BoardClass.py
@@ -9,3 +9,39 @@ class Board:
 
     def get_board(self):
         return self._tiles
+
+    def print_board(self, show_cordinates=True):
+        # Underline and underline_end character modifier sequences
+        ul = "\033[4m"
+        ule = "\033[0m"
+
+        # Print first row with cordinates
+        if show_cordinates:
+            first_number = True
+            last_number = False
+            print(ul + " |" + ule, end='')
+            for column in range(self._size):
+                separator = '|' if not last_number else ' '
+                if first_number:
+                    separator = ''
+                    first_number = False
+                print(ul + "{}{}".format(separator, column) + ule, end='')
+                if column == (self._size - 1):
+                    print(ul + " " + ule, end='')
+            print()
+
+        # Print each individual row
+        for row in range(self._size):
+            start = True
+            for column in range(self._size):
+                char = " " if (self._tiles[row][column] is None) else (self._tiles[row][column])
+                separator = '│' if not start else ''
+                if start:
+                    start = False
+                    # Add row number if show_cordinates enabled
+                    if show_cordinates:
+                        print("{}│".format(row), end='')
+                print(ul + "{}{}".format(separator, char) + ule, end='')
+                if column == (self._size - 1):
+                    print("│", end='')
+            print()
